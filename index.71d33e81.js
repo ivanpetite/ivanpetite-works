a1f01fdb04fd45e598dff13ab1db4f27f12eb668
ivan_petite <diocro1@gmail.com>
2025-01-13 17:01:28 +0100

    nextts

--- index.71d33e81.js
+++ index.71d33e81.js
@@ -600,12 +600,18 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 let button = document.querySelector('#btn_close');
 let container = document.querySelector('#main_share');
 let contact2 = document.querySelector('.contact_2');
+let contact3 = document.querySelector('.contact_3');
 // Add class to the element
 button.addEventListener('click', function() {
     container.classList.add('is_not_displayed');
 });
 // Remove class to the element
 contact2.addEventListener('click', function() {
+    container.classList.remove('is_not_displayed');
+    container.classList.remove('main_bg_share');
+    container.classList.toggle('is_really_displayed');
+});
+contact3.addEventListener('click', function() {
     container.classList.remove('is_not_displayed');
     container.classList.remove('main_bg_share');
     container.classList.toggle('is_really_displayed');
