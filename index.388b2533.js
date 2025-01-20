398fdef0c5665b1ce87b2e4ea89f13bb30499fa5
ivan_petite <diocro1@gmail.com>
2025-01-20 15:50:44 +0100

    newws

---
+++ index.388b2533.js
@@ -0,0 +1,2 @@
+var n;n=window.scrollY,window.onscroll=function(){var e=window.scrollY;n>e?document.getElementById("menu_main2").style.top="0":document.getElementById("menu_main2").style.top="-50px",n=e};
+//# sourceMappingURL=index.388b2533.js.map
---
+++ index.8c77d498.js
@@ -0,0 +1,2 @@
+var n=window.scrollY;window.onscroll=function(){var e=window.scrollY;n>e?document.getElementById("menu_main2").style.top="0":document.getElementById("menu_main2").style.top="-50px",n=e};
+//# sourceMappingURL=index.8c77d498.js.map
